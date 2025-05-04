68624f1fa7178e08ad8bcd1106f3c20ac26c1155
Richard Blažek <blazek.risa@gmail.com>
2025-05-04 21:08:15 +0800

    files

---
+++ main.py
@@ -0,0 +1,53 @@
+# Dataset: https://www.kaggle.com/datasets/mubashirrahim/wind-power-generation-data-forecasting
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+
+def load_setup():
+    df = pd.read_csv('Power.csv')
+    df['hour'] = df['Time'].apply(lambda t: int(t[-8:-6]))
+    demand = np.array([
+        0.211, 0.201, 0.195, 0.190, 0.185, 0.195,
+        0.238, 0.264, 0.275, 0.270, 0.264, 0.280,
+        0.275, 0.270, 0.254, 0.264, 0.291, 0.317,
+        0.301, 0.286, 0.270, 0.254, 0.238, 0.222,
+    ])
+    return df, demand
+
+def split_data(df: pd.DataFrame, pred_len: int):
+    return df[:-pred_len], df[-pred_len:]
+
+def extract_xy(df: pd.DataFrame):
+    X = df[['hour','temperature_2m','relativehumidity_2m','windspeed_10m','windspeed_100m','winddirection_10m','winddirection_100m','windgusts_10m','Power']][:-2]
+    y = df['Power'][2:]
+    return X, y
+
+def scale_data(X_train, X_pred):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_pred = scaler.transform(X_pred)
+    return X_train, X_pred
+
+def train_mlp_regressor(X_train, y_train):
+    model = MLPRegressor((128, 64, 32), max_iter=10000, random_state=42, early_stopping=True)
+    model.fit(X_train, y_train)
+    return model
+
+df, demand = load_setup()
+df_train, df_pred = split_data(df, 720)
+
+X_train, y_train = extract_xy(df_train)
+X_pred, y_pred = extract_xy(df_pred)
+X_train, X_pred = scale_data(X_train, X_pred)
+
+regressor = train_mlp_regressor(X_train, y_train)
+
+y_hat = regressor.predict(X_pred)
+print('R-squared:', r2_score(y_pred, y_hat))
+
+plt.plot(list(zip(y_pred, y_hat)))
+plt.show()
